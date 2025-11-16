8d8189ee7300404c9c476e103ed2291313034401
BooleanCube <samarthupadhya2309@gmail.com>
2025-11-16 12:10:34 -0500

    cf round 1064

---
+++ codeforces/1064/b.py
@@ -0,0 +1,35 @@
+import random
+import math
+from collections import defaultdict, Counter, deque, OrderedDict
+from queue import PriorityQueue
+from heapq import heapify, heappush, heappop
+from functools import lru_cache, reduce
+from bisect import bisect_left, bisect_right
+from types import GeneratorType
+import sys
+
+MOD = 10**9+7
+HMOD = 998244353
+MAXN = 10**5+5
+INF = 1e20
+EPS = 1e-9
+
+input = lambda : sys.stdin.readline().strip()
+print = lambda *args : sys.stdout.write(" ".join(map(str, args)) + "\n")
+write = lambda *args : sys.stdout.write(" ".join(map(str, args)))
+
+getint = lambda : int(input())
+getlist = lambda : list(map(int, input().split()))
+getstr = lambda : list(input()) # mutable string
+
+def solve():
+    a, b, n = getlist()
+    if a / n < b: print(2 - int(a == b))
+    else: print(1)
+
+testcases = 1
+testcases = getint()
+for c in range(1, testcases+1):
+    #write(f"Case {c}: ")
+    solve()
+
---
+++ codeforces/1064/e.cpp
@@ -0,0 +1,98 @@
+// WA
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+/* TYPES  */
+#define ll long long
+#define ld long double
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+/* CONSTANTS */
+#define f first
+#define s second
+#define sp <<" "<<
+#define endl '\n'
+const int MAXN = 1e5+5;
+const ll MOD = 1e9+7;
+const ll HMOD = 998244353;
+const ll INF = 1e9;
+const ld PI = 3.1415926535897932384626433832795;
+const ld EPS = 1e-9;
+
+/* UTILS */
+#define read(type) readInt<type>()
+ll min(ll a,int b) { if (a<b) return a; return b; }
+ll min(int a,ll b) { if (a<b) return a; return b; }
+ll max(ll a,int b) { if (a>b) return a; return b; }
+ll max(int a,ll b) { if (a>b) return a; return b; }
+ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
+string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
+bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
+void yes() { cout<<"YES\n"; }
+void no() { cout<<"NO\n"; }
+
+/* FUNCTIONS */
+#define sz(a) ((int)a.size())
+#define all(a) (a).begin(), (a).end()
+#define fr(i,s,e) for(ll i=(s);i<(e);i++)
+#define frn(i,n) fr(i,0,(n))
+#define cfr(i,s,e) for(ll i=(s);i<=(e);i++)
+#define rfr(i,e,s) for(ll i=(e)-1;i>=(s);i--)
+#define afr(a) for(auto &u:a)
+#define pb push_back
+#define eb emplace_back
+
+/* DEBUGGING && PRINTING */
+#define printv(a) {for(auto u:a) cout<<u<<" "; cout<<endl;}
+#define printm(a) {for(auto u:a) cout<<u.f sp u.s<<endl;}
+
+/*  All Required define Pre-Processors and typedef Constants */
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+
+
+#define int ll
+
+void solve() {
+  int n, q; cin >> n >> q;
+  vi a(n); afr(a) cin >> u;
+  sort(all(a)); reverse(all(a));
+  while(q--) {
+    int c, ans = 0, i = 0; cin >> c;
+    while(c) {
+      int v = 1 << (ll)ceil(__lg(c));
+      // cout << i sp a[i] sp v << endl;
+      while(a[i] > v && c ^ v) v ^= 1 << (ll)ceil(__lg(c ^ v));
+      // cout << i sp a[i] sp v << endl;
+      if(i == sz(a)-1) v = c;
+      ans += max(0, v - a[i]);
+      c ^= v; i++;
+    }
+    cout << ans << endl;
+  }
+}
+
+signed main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0); cout.tie(0);
+
+  int tc = 1;
+  cin >> tc;
+  cfr(t, 1, tc) {
+    // cout << "Case #" << t << ": ";
+    solve();
+  }
+}
+
